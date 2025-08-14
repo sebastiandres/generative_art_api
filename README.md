f31e4748804069bfb80e23c02408c802343ca839
Sebastian Flores <sebastian.flores@u-planner.com>
2025-08-14 12:41:16 -0400

    small tweaks

--- README.md
+++ README.md
@@ -18,4 +18,8 @@ uvicorn main:app --host 0.0.0.0 --port $PORT
 
 ## Online
 
-I've deployed it to render.com 
+I've used render.com to deploy it to [generative-art-api.onrender.com](https://generative-art-api.onrender.com)
+
+## Swagger UI
+
+As usual, the swagger is on the [/docs endpoint](https://generative-art-api.onrender.com/docs).
--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Generative art"}
+    return {"help": "See /docs for more information"}
+
+@app.get("/help")
+def help():
+    return {"help": "See /docs for more information"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
